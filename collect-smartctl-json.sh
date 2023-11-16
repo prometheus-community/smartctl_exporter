8193e8eb3b7407ea9cb44af18fe52bcdf9463c16
David Randall <David@NiceGuyIT.biz>
2023-11-15 20:35:53 -0500

    Chg: Impvoe the JSON collection script; now requires jq/yq
    Signed-off-by: David Randall <David@NiceGuyIT.biz>

---
+++ collect-smartctl-json.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Data directory to dump smartctl output
+# This directory will be created if it doesn't exist
+data_dir="${script_dir}/smartctl-data"
+
+# The original script used --xall but that doesn't work
+# This matches the command in readSMARTctl()
+smartctl_args="--json --info --health --attributes --tolerance=verypermissive --nocheck=standby --format=brief --log=error"
+
+# Determine the json query tool to use
+if command -v jq >/dev/null; then
+	json_tool="jq"
+	json_args="--raw-output"
+elif command -v yq >/dev/null; then
+	json_tool="yq"
+	json_args="--unwrapScalar"
+else
+	echo "One of 'yq' or 'jq' is required. Please try again after installing one of them"
+	exit 1
+fi
+
+if [[ $UID != 0 ]] && ! command -v sudo >/dev/null; then
+	# Not root and sudo doesn't exist
+	echo "sudo does not exist. Please run this as root"
+	exit 1
+fi
+
+SUDO="sudo"
+if [[ $UID = 0 ]]; then
+	# Don't use sudo if root
+	SUDO=""
+fi
+
+[[ ! -d "${data_dir}" ]] && mkdir --parents "${data_dir}"
+
+if [[ $# -ne 0 ]]; then
+	devices="$1"
+else
+	devices=$(smartctl --scan --json | $json_tool $json_args '.devices[].name')
+fi
+
+for device in $devices; do
+	echo -n "Collecting data for '${device}'..."
+	data=$($SUDO smartctl $smartctl_args "${device}")
+	type=$(echo "${data}" | $json_tool $json_args '.device.type')
+	family=$(echo "${data}" | $json_tool $json_args '.model_family' | tr ' ' '_')
+	model=$(echo "${data}" | $json_tool $json_args '.model_name' | tr ' ' '_')
+	device_name=$(basename "${device}")
+	echo -e "\tSaving to ${type}-${family}-${model}-${device_name}.json"
+	echo "${data}" > "${data_dir}/${type}-${family}-${model}-${device_name}.json"
+done
